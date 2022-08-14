5b52db0ed720712336a92a784eb2fbd4f95d1403
andreasmm16 <56442641+andreasmm16@users.noreply.github.com>
2022-08-13 22:53:54 -0600

    Add files via upload

---
+++ backend/backend/index.js
@@ -0,0 +1,10 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+const port = process.env.PORT | 4000
+
+const app = express()
+app.use(express.json())
+
+app.listen(port, console.log(`Servidor abierto en el puerto ${port}`))
