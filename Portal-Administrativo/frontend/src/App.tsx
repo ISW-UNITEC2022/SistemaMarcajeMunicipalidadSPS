89075281664c4b186ac8cb58f385736b4a97dcf6
BernardoMartinez08 <bernardo.martinez@unitec.edu>
2022-08-17 11:14:34 -0600

    Login con Auth0
    Primera version del Login con Auth0, pendiente modificar los botones y opciones que se presentan en la pantalla de inicio de sesiom.
    
    En desarrollo.........

--- Portal-Administrativo/frontend/src/App.tsx
+++ Portal-Administrativo/frontend/src/App.tsx
@@ -1,24 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import LoginButton from './components/LoginButton'
+import LogoutButton from './components/LogoutButton'
+import Profile from './components/Profile'
+import { useAuth0 } from '@auth0/auth0-react'
+import './components/styles.css'
 
 function App() {
+  const { isAuthenticated , isLoading} = useAuth0()
+
+  if (isLoading){
+    return (
+      <h1>
+        Estamos cargando los datos de tu perfil a nuestra aplicacion.
+      </h1>
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h3>
+        Panel de control
+      </h3>
+
+      {
+        isAuthenticated ? <LogoutButton></LogoutButton> : <LoginButton></LoginButton>
+      }
+
+      <br></br>
+      <Profile></Profile>
     </div>
   );
 }
