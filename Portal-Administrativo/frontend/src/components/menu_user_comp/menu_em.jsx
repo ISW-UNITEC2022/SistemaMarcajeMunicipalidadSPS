338a413916a91cf42740143d20b2d89fbf229a27
DHK2001 <dhenrygk12@gmail.com>
2022-08-19 06:55:51 -0600

    Componente menu empleado y reporte
    Componentes menu empleado y reportes. En total 6 archivos. menu empleado y menu reporte con sus hojas de estilo, el menu_index donde se pusieron ambos componentes para probarlo y una hoja de estilo utilizado por los 2 componentes.

---
+++ Portal-Administrativo/frontend/src/components/menu_user_comp/menu_em.jsx
@@ -0,0 +1,20 @@
+import './menu_em.css';
+import './menu.css';
+
+function DropdownM(props){
+
+return(
+    <div className={`ventana ${props.children==false ? 'hide' : ''}`} >
+
+        <div>
+            <a href="" ><div className='sub'><div className='point'><i class="fa-solid fa-circle"></i></div>Crear nuevo empleado</div></a> 
+            <a href="" ><div className='sub'><div className='point'><i class="fa-solid fa-circle"></i></div>Modificar empleado</div></a> 
+            <a href="" ><div className='sub'><div className='point'><i class="fa-solid fa-circle"></i></div>Informacion de empleados</div></a> 
+            <a href="" ><div className='sub'><div className='point'><i class="fa-solid fa-circle"></i></div>Estado empleados</div></a> 
+        </div>
+        
+    </div>
+)
+
+};
+export default DropdownM;
---
+++ Portal-Administrativo/frontend/src/components/menu_user_comp/menu_re.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './menu_re.css';
+import './menu.css';
+
+function DropdownM(props){
+
+return(
+    <div className={`ventana2 ${props.children==false ? 'hide' : ''}`} >
+
+        <div>
+            <a href="" ><div className='sub'><div className='point'><i class="fa-solid fa-circle"></i></div>Reportes de  Asistencia</div></a> 
+            <a href="" ><div className='sub'><div className='point'><i class="fa-solid fa-circle"></i></div>Reportes de Tardias</div></a> 
+        </div>
+        
+    </div>
+)
+
+};
+export default DropdownM;
