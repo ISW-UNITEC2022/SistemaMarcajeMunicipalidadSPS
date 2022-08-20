83446da62166759786554b2e278d5b9bab7dda47
BernardoMartinez08 <bernardo.martinez@unitec.edu>
2022-08-20 08:03:32 -0600

    Home Page y Login
    Se oficializo el uso de homepage.tsx, este pasa a sustituir el app.tsx que estab ausandose. El anterior queda como archivo por defecto.

--- Portal-Administrativo/frontend/src/App.tsx
+++ Portal-Administrativo/frontend/src/App.tsx
@@ -1,35 +1,9 @@
 import React from 'react';
 import './App.css';
-import LoginButton from './components/HomePage/Botones/LoginButton'
-import LogoutButton from './components/HomePage/Botones/LogoutButton'
-import Profile from './auth0/Profile'
-import { useAuth0 } from '@auth0/auth0-react'
-
 
 function App() {
-  const { isAuthenticated , isLoading} = useAuth0()
-
-  if (isLoading){
-    return (
-      <h1>
-        Estamos cargando los datos de tu perfil a nuestra aplicacion.
-      </h1>
-    )
-  }
-
   return (
     <div className="App">
-
-      <h3>
-        Panel de control
-      </h3>
-
-      {
-        isAuthenticated ? <LogoutButton></LogoutButton> : <LoginButton></LoginButton>
-      }
-
-      <Profile></Profile>
-
       
     </div>
   );
--- Portal-Administrativo/frontend/src/index.tsx
+++ Portal-Administrativo/frontend/src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import Login from './auth0/Login';
 import reportWebVitals from './reportWebVitals';
 
