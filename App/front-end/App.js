1ee147f544d08ddd4a0c8b043725bd20705217f5
Angel Guevara <42151778+GuevaraUclesHN@users.noreply.github.com>
2022-08-22 12:40:21 -0600

    Pantalla

--- App/front-end/App.js
+++ App/front-end/App.js
@@ -1,21 +1,122 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { StyleSheet, Text, AppRegistry ,View,Button,Dimensions,Image,TextInput,TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+       <View style={styles.shape_conatiner}>
+     <View style={[styles.square,{
+      backgroundColor: '#02732A'
+     }]}/>
+     <View style={[styles.square,{
+      backgroundColor: '#F2B705'
+     }]}/>
+     <View style={[styles.square,{
+      backgroundColor: '#BF0404'
+     }]}/>
+  
+     </View>
+
+     <Image
+          style={{
+            resizeMode: 'contain',
+            height: 150,
+            width: 121,
+            left: 228,  
+            top: 32,
+          }}
+        source = {require('./assets/logo.png')}
+      />
+      <View style={styles.botton}></View>
+      <View style={styles.content}>
+            <Text style={styles.texto}> Iniciar Sesión </Text>
+            
+            <TextInput  style={styles.input}
+            placeholder='Correo electrónico'></TextInput>
+              <TextInput secureTextEntry={true}  style={styles.input}
+            placeholder='Contraseña'></TextInput>
+
+              <TouchableOpacity style={styles.buttonContainer}>
+              <Text style={styles.textbutton}> Ingresar </Text>
+              </TouchableOpacity>
+      </View> 
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex:1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  shape_conatiner:{
+    height: 20,
+    width: 125,
+    backgroundColor: '#013613',
+    flexDirection: 'row',
+    justifyContent:'flex-start',
+    alignItems:'flex-start',
+    
+
+  },
+  square:{
+    height: 20,
+    width: 125,
+  },
+  botton:{
+    height: 50,
+    top: 610,
+    width: 380,
+    backgroundColor: '#013613',
+  },
+  content:{
+    flex:2,
+    alignContent:'center',
+    
+  },
+  texto:{
+    color:'#1F3821',
+    fontSize: 36,
+    left:61,
+    height: 49,
+    width: 240,
+    top:18,
+    fontWeight:'bold'
+  },
+
+  buttonContainer:{
+    top:50,
+    backgroundColor:'#02732A',
+    width:270,
+    height:47,
+    left:46,
+    marginTop:33,
+    borderRadius:10,
+    textAlign:'center',
+  },
+
+  input: {
+    left:45,
+    top:50,
+    fontSize:18,
+    width:270,
+    height:45,
+    marginBottom:33,
+    backgroundColor:'#80E673',
+    textAlign:'center',
+    borderRadius:10
+  },
+  
+  textbutton:{
+    fontSize:24,
+    top:10,
+    color:'#F2F2F2',
+    textAlign:'center',
+    fontWeight:'bold'
+  }
+  
+
 });
+
+
