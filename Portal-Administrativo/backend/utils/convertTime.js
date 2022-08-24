38fd69b7cb6cde1ed38ee9099252a075e439ed83
SamuelRocca85 <samuelandres_rocha@unitec.edu>
2022-08-23 21:33:36 -0600

    Se agrego entrada y salida a los empleados, se modifico middleware de error y se modifico ruta de autenticacion

---
+++ Portal-Administrativo/backend/utils/convertTime.js
@@ -0,0 +1,9 @@
+export const dateToTimeString = (dataD) => {
+  let data = new Date(dataD)
+  let hrs = data.getHours()
+  let mins = data.getMinutes()
+  if (hrs <= 9) hrs = '0' + hrs
+  if (mins < 10) mins = '0' + mins
+  const postTime = hrs + ':' + mins
+  return postTime
+}
