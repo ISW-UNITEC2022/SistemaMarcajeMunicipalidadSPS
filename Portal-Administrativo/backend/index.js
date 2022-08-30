c6642e186fd12038252540daf2d82eb801d81920
SamuelRocca85 <samuelandres_rocha@unitec.edu>
2022-08-29 21:26:37 -0600

    arregle typo

--- Portal-Administrativo/backend/index.js
+++ Portal-Administrativo/backend/index.js
@@ -7,7 +7,7 @@ import rutasSupervisores from './routes/supervisores.js'
 import favicon from 'serve-favicon'
 
 dotenv.config()
-const port = process.env.PORT | 4000
+const port = process.env.PORT || 4000
 
 const app = express()
 app.use(express.json())
