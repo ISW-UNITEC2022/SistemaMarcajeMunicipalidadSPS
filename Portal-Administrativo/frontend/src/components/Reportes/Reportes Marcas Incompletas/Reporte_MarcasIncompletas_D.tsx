7b932625101c117a9eb6fac3bdea97a0d3c24e14
DHK2001 <64647310+DHK2001@users.noreply.github.com>
2022-11-24 10:53:34 -0600

    fecha

--- Portal-Administrativo/frontend/src/components/Reportes/Reportes Marcas Incompletas/Reporte_MarcasIncompletas_D.tsx
+++ Portal-Administrativo/frontend/src/components/Reportes/Reportes Marcas Incompletas/Reporte_MarcasIncompletas_D.tsx
@@ -5,30 +5,30 @@ import {Document, Page, View, Text, Image, PDFViewer, StyleSheet} from "@react-p
 
 const getMes=(mes)=>{
   switch(mes){
-    case 'Enero':
-      return 1;
-      case 'Febrero':
-        return 2;
-      case 'Marzo':
-        return 3;
-      case 'Abril':
-        return 4;
-      case 'Mayo':
-        return 5;
-      case 'Junio':
-        return 6;
+    case 1:
+      return 'Enero';
+      case 2:
+        return 'Febrero';
+      case 3:
+        return 'Marzo';
+      case 4:
+        return 'Abril';
+      case 5:
+        return 'Mayo';
+      case 6:
+        return 'Junio';
+      case 7:
+        return 'Julio';
       case 8:
-        return 7;
-      case 'Agosto':
-        return 8;
-      case 'Septiembre':
-        return 9;
-      case 'Octubre':
-        return 10;
-      case 'Noviembre':
-        return 11;
-      case 'Diciembre':
-        return 12;
+        return 'Agosto';
+      case 9:
+        return 'Septiembre';
+      case 10:
+        return 'Octubre';
+      case 11:
+        return 'Noviembre';
+      case 12:
+        return 'Diciembre';
       default:
         return '---';
   }
