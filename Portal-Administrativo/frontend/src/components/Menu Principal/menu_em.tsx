680befa510df1d06fa28e8f7db2ec1f60a22f65e
DHK2001 <dhenrygk12@gmail.com>
2022-12-14 15:22:10 -0600

    asignar supervisor

--- Portal-Administrativo/frontend/src/components/Menu Principal/menu_em.tsx
+++ Portal-Administrativo/frontend/src/components/Menu Principal/menu_em.tsx
@@ -1,28 +1,43 @@
 import './menu_em.css';
 import './menu.css';
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+import { useAuth0 } from '@auth0/auth0-react'
 
 function DropdownM(props){
 
-const esAdmin = (rol)=>{
+const {user} = useAuth0();
 
-    if(rol=="Administrador")
-        return true;
+const url = "https://proyecto-isw1.herokuapp.com/api/supervisores/"+user.sub;
+
+const [supervisor, setS] = useState(false);
+
+const esAdmin = async()=>{
+
+    const response = await fetch(url)
+    const data = await response.json()
+
+    if(data.idempleado==='1234-5678-91011')
+        setS(true)
     else
-        return false;
+        setS(false)
 
 }
 
-
+useEffect(() => {
+    esAdmin();
+  }, []);
+  
 return(
+    
     <div className={`ventana ${props.children==false ? 'hide' : ''}`} >
-
+        
         <div>
             <a href="/crear_empleado"><div id='sub'><div id='point'><i className="fa-solid fa-circle"></i></div>Crear nuevo empleado</div></a> 
             <a href="/modificar_empleado" ><div id='sub'><div id='point'><i className="fa-solid fa-circle"></i></div>Modificar empleado</div></a> 
             <a href="/informacion_empleado" ><div id='sub'><div id='point'><i className="fa-solid fa-circle"></i></div>Informacion de empleados</div></a> 
             <a href="/dar_baja_alta" ><div id='sub'><div id='point'><i className="fa-solid fa-circle"></i></div>Estado empleados</div></a> 
-            <a href="/asignar_supervisor" ><div id='sub'>{esAdmin("Administrador") ? <div id='point'><i className="fa-solid fa-circle"></i></div>:''}{esAdmin("Administrador") ? 'Asignar Supervisor':''}</div></a> 
+            <a href="/asignar_supervisor" ><div id='sub'>{supervisor ? <div id='point'><i className="fa-solid fa-circle"></i></div>:''}{supervisor ? 'Asignar Supervisor':''}</div></a> 
         </div>
     </div>
 )
